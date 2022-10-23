fcc667faceda985fc1cb2d6e32df2d66c299d187
Sarak Dahal <ssagamersdignity@gmail.com>
2022-10-23 07:22:19 +0545

    1st

--- app/user.py
+++ app/user.py
@@ -1,20 +1,22 @@
-# Importing required libraries
 import datetime
-import os
 import time
 import pandas as pd
 from numpy.core.defchararray import strip
+import os
 from app import app
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from flask import render_template, request
-
+from collections import OrderedDict
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+
+global appended_data
+appended_data = []
 
 
 @app.route('/')
@@ -27,18 +29,15 @@ def upload_file():
     return render_template('upload.html')
 
 
-# Creating List variable
 list2 = []
 
 
 @app.route('/uploader', methods=['GET', 'POST'])
 def uploadfile():
-
     if request.method == 'POST':
-        # Getting upload file
         f = request.files['file']
         f.save(f.filename)
-        # Getting required data
+
         country = request.form['c']
         time = request.form['t']
         time = int(time)
@@ -46,7 +45,6 @@ def uploadfile():
         with open('upload.csv', encoding='utf-8-sig') as f:
             for row in f:
                 list2.append(row)
-        # Making loop to run according to variable
         for i in range(len(list2)):
             print(list2[i])
             keyword = list2[i]
@@ -59,13 +57,11 @@ def uploadfile():
 @app.route('/download', methods=['GET', 'POST'])
 def download():
     if request.method == 'POST' or request.method == 'GET':
-        # Adding required variable from html form
         country = request.form['c']
         keyword = request.form['k']
         time = request.form['t']
         time = int(time)
 
-        # Calling method to perform certain value
         x = FBAdsScraper(country, keyword, time)
         x.scrape_ads()
     else:
@@ -74,11 +70,9 @@ def download():
     return render_template('index.html')
 
 
-# Creating list data
 data = []
 
 
-# Creating class which includes methods inside of it
 class FBAdsScraper:
 
     def __init__(self, country, keyword, time):
@@ -90,32 +84,27 @@ class FBAdsScraper:
 
         self.ads = []
 
+        self.driver = driver
 
     def scrape_ads(self):
-        # Making fb scraper link
-        driver.get(
+
+        self.driver.get(
             "https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country={}&q={}".format(
                 self.country,
                 self.keyword))
-        driver.implicitly_wait(1)
+        self.driver.implicitly_wait(1)
         time.sleep(1)
 
-
-        # Setting time with user input
         endTime = datetime.datetime.now() + datetime.timedelta(seconds=self.time)
-
-        # Making automatic scroll
         while True:
             if datetime.datetime.now() >= endTime:
                 break
             else:
-                height = driver.execute_script("return document.documentElement.scrollHeight")
-                driver.execute_script("window.scrollTo(0, " + str(height) + ");")
+                height = self.driver.execute_script("return document.documentElement.scrollHeight")
+                self.driver.execute_script("window.scrollTo(0, " + str(height) + ");")
 
-        ads = driver.find_element(by=By.CLASS_NAME, value='_8n_0').find_elements(by=By.CLASS_NAME, value='_99s6')
+        ads = self.driver.find_element(by=By.CLASS_NAME, value='_8n_0').find_elements(by=By.CLASS_NAME, value='_99s5')
         i = 1
-
-        # Using for loop to go through each scanned data
         for ad in ads:
             try:
                 status = ad.find_element(by=By.CLASS_NAME, value='fxk3tzhb').text
@@ -155,8 +144,6 @@ class FBAdsScraper:
             except:
                 video = ""
 
-
-            # Appending data according to the scan result
             data.append({
                 "Keyword": strip(self.keyword),
                 "Status": status,
@@ -170,10 +157,11 @@ class FBAdsScraper:
                 "Video": video
             })
 
+            # ad_details = OrderedDict({"Status": status, "Page": page, "Title": title, "Link": link, "Id": adsId, "Date":date,"Facebook Link":fbLink, "Image": image, "Video": video})
+            # self.ads.append('ad_details')
+            # print(i, ad_details)
             print(i, " Data stored.")
+            # df = df.append(ad_details,ignore_index=True)
             i = i + 1
-        # Storing data to pandas dataframe and converting it to csv file.
+        # df.to_csv('data.csv', sep=',')
         pd.DataFrame(data).to_csv('data.csv')
-
-# END
-# DEVELOPED WITH LOVE BY SARAK DAHAL
